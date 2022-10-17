e0099627a9f6419bfb3e62f2a3d4d4e2e69ba7aa
haruchika-kurosawa <bobmarimotobe@gmail.com>
2022-10-17 23:43:58 +0900

    Update Input01.js

--- src/components/Input01.js
+++ src/components/Input01.js
@@ -2,12 +2,15 @@ import React from "react";
 import {useInput} from '../hooks/useInput';
 
 export default function Input01() {
-	const [inputProps, resetInput] = useInput();
+	const [inputProps, resetInput] = useInput('initial');
 	return (
 		<>
 			<input 
 				{...inputProps}
 			/>
+			<button 
+				onClick={resetInput}
+			>reset</button>
 		</>
 	);
 }
